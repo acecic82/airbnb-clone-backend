df0cab72f981ee0bd87de97eea7054f5f0fdae52
hungrytime <nothing>
2024-06-30 09:10:50 +0900

    houses model 추가

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@shygt6+_sa_vtlgyo9p38uzlov0_3+z$$%8_8=k$^+0fj+=ye'
+SECRET_KEY = 'django-insecure-zjjw52l^p8mg6o-$xo1hjrmq6_06jwf%t&f-a7!&(0sy=vk_t('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'houses.apps.HousesConfig'
 ]
 
 MIDDLEWARE = [
--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Houses():
+
+    """ Model Definition for Houses """
+
+    name = models.CharField( max_length=140 )
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField( max_length=140 )
