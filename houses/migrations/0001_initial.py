8bbe919aa10654eeae58ae1100c73d322ed3ee3a
hungrytime <nothing>
2024-06-30 15:42:22 +0900

    black formatter 설정 추가

--- houses/migrations/0001_initial.py
+++ houses/migrations/0001_initial.py
@@ -7,18 +7,25 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='House',
+            name="House",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=140)),
-                ('price', models.PositiveIntegerField()),
-                ('description', models.TextField()),
-                ('address', models.CharField(max_length=140)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=140)),
+                ("price", models.PositiveIntegerField()),
+                ("description", models.TextField()),
+                ("address", models.CharField(max_length=140)),
             ],
         ),
     ]
--- houses/migrations/0002_rename_price_house_price_per_night_and_more.py
+++ houses/migrations/0002_rename_price_house_price_per_night_and_more.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('houses', '0001_initial'),
+        ("houses", "0001_initial"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='house',
-            old_name='price',
-            new_name='price_per_night',
+            model_name="house",
+            old_name="price",
+            new_name="price_per_night",
         ),
         migrations.AddField(
-            model_name='house',
-            name='pets_allowed',
+            model_name="house",
+            name="pets_allowed",
             field=models.BooleanField(default=True),
         ),
     ]
