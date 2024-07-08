9a2d289ece31152a4b71a4c58a3ef579c9afe859
hungrytime <nothing>
2024-07-09 07:39:58 +0900

    media model and admin and direct_message start app

--- medias/models.py
+++ medias/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+from common.models import CommonModel
+
 # Create your models here.
+
+
+class Photo(CommonModel):
+    """Photo Model Definition"""
+
+    file = models.ImageField()
+    description = models.CharField(
+        max_length=140,
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return "Photo file"
+
+
+class Video(CommonModel):
+    """Video Model Definition"""
+
+    file = models.FileField()
+    experience = models.OneToOneField(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "Photo file"
