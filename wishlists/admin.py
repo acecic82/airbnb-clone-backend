9416676384bede9a13d551e62c4695e2fc5a33cc
hungrytime <nothing>
2024-07-08 13:37:06 +0900

    wishlist admin add

--- wishlists/admin.py
+++ wishlists/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from wishlists.models import WishList
+
 # Register your models here.
+
+
+@admin.register(WishList)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "user",
+        "created_at",
+        "updated_at",
+    )
