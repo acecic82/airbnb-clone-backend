873b630a3928a1090787197c216f668def279a18
hungrytime <nothing>
2024-07-08 10:00:58 +0900

    category apss add

--- experiences/models.py
+++ experiences/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
+from common.models import CommonModel
+
 # Create your models here.
+
+
+class Experience(CommonModel):
+    """Experice Model Definition"""
+
+    country = models.CharField(
+        max_length=50,
+        default="Korea",
+    )
+    city = models.CharField(
+        max_length=80,
+        default="Seoul",
+    )
+    name = models.CharField(
+        max_length=250,
+    )
+    host = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+    price = models.PositiveIntegerField()
+    addrss = models.CharField(
+        max_length=250,
+    )
+    start = models.TimeField()
+    end = models.TimeField()
+    description = models.TextField()
+    perks = models.ManyToManyField(
+        "experiences.Perk",
+    )
+
+
+class Perk(CommonModel):
+    """Perk Model Definition"""
+
+    name = models.CharField(
+        max_length=100,
+    )
+    details = models.CharField(
+        max_length=250,
+        blank=True,
+        default="",
+    )
+    explanation = models.TextField(
+        blank=True,
+        default="",
+    )
