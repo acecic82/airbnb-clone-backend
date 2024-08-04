9ab6a0fa432756805c2dc93b933a5877cb64a9e2
hungrytime <nothing>
2024-08-04 21:36:48 +0900

    apply ModelViewSet

--- categories/urls.py
+++ categories/urls.py
@@ -3,6 +3,23 @@ from categories import views
 
 
 urlpatterns = [
-    path("", views.Categories.as_view()),
-    path("<int:category_id>", views.CategoryDetail.as_view()),
+    path(
+        "",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+    ),
+    path(
+        "<int:pk>",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),
 ]
