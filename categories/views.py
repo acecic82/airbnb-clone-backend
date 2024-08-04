e001569c1507046603af20af1513c289475cd122
hungrytime <nothing>
2024-08-04 20:29:08 +0900

    category view and serializer

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from categories.models import Category
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
+from .serializers import CategorySerializer
 
 # Create your views here.
+
+
+class Categories(APIView):
+    def get(self, request):
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(
+            all_categories,
+            many=True,
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+
+        if serializer.is_valid():
+            new_category = serializer.save()
+            return Response(
+                CategorySerializer(new_category).data,
+            )
+        else:
+            return Response(serializer.errors)
+
+
+class CategoryDetail(APIView):
+    def get_object(self, category_id):
+        try:
+            category = Category.objects.get(pk=category_id)
+        except Category.DoesNotExist:
+            raise NotFound
+
+        return category
+
+    def get(self, request, category_id):
+        serializer = CategorySerializer(self.get_object(category_id))
+        return Response(serializer.data)
+
+    def put(self, request, category_id):
+        serializer = CategorySerializer(
+            self.get_object(category_id),
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, category_id):
+        self.get_object(category_id).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,38 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from rooms.models import Room
+
 # Create your views here.
+
+
+def see_all_room(request):
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            "rooms": rooms,
+            "title": "Hello! this title comes from django!",
+        },
+    )
+
+
+def see_one_room(request, room_id):
+    try:
+        room = Room.objects.get(pk=room_id)
+        return render(
+            request,
+            "room_detail.html",
+            {
+                "room": room,
+            },
+        )
+    except Room.DoesNotExist:
+        return render(
+            request,
+            "room_detail.html",
+            {
+                "not_found": True,
+            },
+        )
