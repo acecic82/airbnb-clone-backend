95e37b47f43639668408203cfa3caf12eaee0331
hungrytime <nothing>
2024-08-17 02:16:37 +0900

    get and put myself method add

--- users/views.py
+++ users/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+from . import serializers
+
 # Create your views here.
+
+
+class Me(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = serializers.PrivateUserSerializer(user)
+
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = request.user
+        serializer = serializers.PrivateUserSerializer(
+            user,
+            data=request.data,
+            partial=True,
+        )
+
+        if serializer.is_valid():
+            savedUser = serializer.save()
+            return Response(serializers.PrivateUserSerializer(savedUser).data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        serializer = serializers.PrivateUserSerializer(
+            data=request.data,
+        )
+
+        if serializer.is_valid():
+            pass
+        else:
+            return Response(serializer.errors)
