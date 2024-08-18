000a35b47662bb22d98ccc88afc623a6bcc2ff5f
acecic82 <102938al@naver.com>
2024-08-19 03:27:25 +0900

    isOwner, isLiked add using graphQL

--- rooms/types.py
+++ rooms/types.py
@@ -1,8 +1,11 @@
 from config import settings
 import strawberry
+from strawberry.types import Info
 import typing
 from strawberry import auto
+
 from . import models
+from wishlists.models import WishList
 from users.types import UserType
 from reviews.types import ReviewType
 
@@ -15,7 +18,7 @@ class RoomType:
     owner: "UserType"
 
     @strawberry.field
-    def reviews(self, page: int) -> typing.List["ReviewType"]:
+    def reviews(self, page: typing.Optional[int] = 1) -> typing.List["ReviewType"]:
         page_size = settings.PAGE_SIZE
         start = (page - 1) * page_size
         end = start + page_size
@@ -24,3 +27,14 @@ class RoomType:
     @strawberry.field
     def rating(self) -> str:
         return self.rating()
+
+    @strawberry.field
+    def is_owner(self, info: Info) -> bool:
+        return self.owner == info.context.request.user
+
+    @strawberry.field
+    def is_liked(self, info: Info) -> bool:
+        return WishList.objects.filter(
+            user=info.context.request.user,
+            rooms__pk=self.pk,
+        ).exists()
