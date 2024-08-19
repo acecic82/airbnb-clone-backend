16f214ec4d86c145899ea10651d2f1fbeca4d5f6
acecic82 <102938al@naver.com>
2024-08-20 03:06:41 +0900

    login add with graphQL

--- rooms/schema.py
+++ rooms/schema.py
@@ -1,5 +1,7 @@
 import strawberry
 import typing
+
+from config.permissions import OnlyLoggedIn
 from . import types
 from . import queries
 
@@ -7,7 +9,8 @@ from . import queries
 @strawberry.type
 class Query:
     all_rooms: typing.List[types.RoomType] = strawberry.field(
-        resolver=queries.get_all_rooms
+        resolver=queries.get_all_rooms,
+        permission_classes=[OnlyLoggedIn],
     )
 
     room: typing.Optional[types.RoomType] = strawberry.field(
