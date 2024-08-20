0853dbff9de084a06656a6a5b7d037917e47a860
acecic82 <102938al@naver.com>
2024-08-21 05:28:31 +0900

    make token and login using rest_framework

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
@@ -6,6 +7,7 @@ urlpatterns = [
     path("me", views.Me.as_view()),
     path("log-in", views.LogIn.as_view()),
     path("log-out", views.LogOut.as_view()),
+    path("token-login", obtain_auth_token),
     path("change-password", views.ChangePassword.as_view()),
     # 순서에 따라 me를 username 으로 인식할 수도 있음
     path("@<str:username>", views.PublicUser.as_view()),
